dd4aa2ca52ac7c6c16cd37ef4c6b128897837582
NGUYEN DANG THANH <d_thanh@brycen-vn.local>
2019-10-08 09:01:52 +0700

    all of hello work source

---
+++ src/main/java/com/thanh/HelloThanhApplication.java
@@ -0,0 +1,13 @@
+package com.thanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloThanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloThanhApplication.class, args);
+	}
+
+}
